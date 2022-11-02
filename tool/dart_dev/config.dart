bc9767764f1bfe9ca4eb27911fa860f5938c6a7b
Max Peterson <maxwell.peterson@webfilings.com>
2022-11-01 22:37:36 -0500

    Remove over_react_format

--- tool/dart_dev/config.dart
+++ tool/dart_dev/config.dart
@@ -1,8 +1,6 @@
 import 'package:dart_dev/dart_dev.dart';
 import 'package:dart_dev_workiva/dart_dev_workiva.dart';
-import 'package:over_react_format/dart_dev_tool.dart';
 
 final Map<String, DevTool> config = {
   ...workivaConfig,
-  'format': OverReactFormatTool(),
 };
