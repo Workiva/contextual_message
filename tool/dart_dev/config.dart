b42e83ce2ce75327fdcab49c5468eb0879e883df
Matthew Nitschke <matthew.nitschke@workiva.com>
2023-10-02 12:34:51 -0600

    updated dart_dev config

--- tool/dart_dev/config.dart
+++ tool/dart_dev/config.dart
@@ -1,6 +1,3 @@
-import 'package:dart_dev/dart_dev.dart';
 import 'package:dart_dev_workiva/dart_dev_workiva.dart';
 
-final Map<String, DevTool> config = {
-  ...workivaConfig,
-};
+final config = workivaConfig;
